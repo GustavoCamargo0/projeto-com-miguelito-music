52597aa1a355540767d0482b4c088fa6f4c2354a
GustavoCamargo0 <gustavo.fscamargo08@gmail.com>
2026-01-04 17:39:47 -0300

    Search para musicas funcionando

--- music/src/App.jsx
+++ music/src/App.jsx
@@ -1,24 +1,41 @@
 import { useState, useEffect } from 'react'
+import Header from './components/Header'
 
 function App() {
    const [tracks, setTracks] = useState([]);
    const [pesquisa, setPesquisa] = useState('');
 
-  useEffect(() => {
-    if (pesquisa.length > 2) {
-      fetch(`https://api.deezer.com/search/track?q=${pesquisa}`)
-        .then(res => res.json())
-        .then(data => {
-          setTracks(data.data)
-        })
-    } else {
-      setTracks([])
-    }
-  }, [pesquisa]);
+useEffect(() => {
+  if (pesquisa.length > 2) {
+    const callbackName = "deezerCallback";
+    window[callbackName] = (data) => {
+      setTracks(data.data);
+      delete window[callbackName];
+    };
+
+    const script = document.createElement("script");
+    script.src = `https://api.deezer.com/search?q=${pesquisa}&output=jsonp&callback=${callbackName}`;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  } else {
+    setTracks([]);
+  }
+}, [pesquisa]);
 
   return (
     <div>
       <Header pesquisa={pesquisa} setPesquisa={setPesquisa} />
+
+      {tracks.map(track => (
+        <div key={track.id}>
+          <img src={track.album.cover_small} alt={track.title} />
+          <p>{track.title} - {track.artist.name}</p>
+        </div>
+      ))}
+
     </div>
   )
 }
